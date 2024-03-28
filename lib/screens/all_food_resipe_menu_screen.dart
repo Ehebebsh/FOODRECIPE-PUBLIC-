c158e316f51e4b8dd6668b851b6799157c5241f1
jh <zao3662@naver.com>
2024-03-28 23:34:28 +0900

    commit

--- lib/screens/all_food_resipe_menu_screen.dart
+++ lib/screens/all_food_resipe_menu_screen.dart
@@ -92,7 +92,7 @@ class _FoodPageState extends State<AllFoodPage> {
                           food['image'],
                           fit: BoxFit.cover,
                           width: double.infinity,
-                          height: 200,
+                          height: 300,
                         ),
                       ),
                       const SizedBox(height: 8.0),
