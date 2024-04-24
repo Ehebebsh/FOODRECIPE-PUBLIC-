a11e97f7dda9781628cec77fb0d72e0fa0e77f1f
songjunha <likesong00@naver.com>
2024-04-24 20:53:21 +0900

    login button design

--- lib/widgets/googleandkakao_widgets.dart
+++ lib/widgets/googleandkakao_widgets.dart
@@ -11,35 +11,38 @@ import '../models/userfirestoreservice.dart';
 import '../provider/user_provider.dart';
 
 class GoogleLoginButton extends StatelessWidget {
-  final VoidCallback onPressed;
+  final VoidCallback? onPressed;
 
-  const GoogleLoginButton({Key? key, required this.onPressed}) : super(key: key);
+  const GoogleLoginButton({Key? key, this.onPressed}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
       onPressed: onPressed,
-      color: Colors.white,
+      color: Colors.white, // 버튼 색상 변경
       elevation: 3,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-        side: const BorderSide(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10), // 모서리를 둥글게 만듦
       ),
-      child: const Padding(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: SizedBox( // 버튼 크기를 조정하기 위해 SizedBox로 감싸기
+        height: 50, // 버튼의 높이 설정
+        width: MediaQuery.of(context).size.width * 0.8, // 화면 너비의 80%로 설정
         child: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Icon(
-              Icons.add_circle_outline,
-              color: Colors.red, // 구글 컬러로 변경 가능
+            SizedBox( // 이미지를 표시하기 위해 SizedBox로 감싸기
+              width: MediaQuery.of(context).size.width * 0.1, // 화면 너비의 10%로 설정
+              height: MediaQuery.of(context).size.width * 0.1, // 화면 너비의 10%로 설정
+              child: Image.asset('assets/googleicon.png'), // 이미지 추가
             ),
-            SizedBox(width: 10),
-            Text(
-              '구글 로그인',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black87,
+            const SizedBox(width: 10), // 이미지와 텍스트 사이의 간격 조정
+            Padding(
+              padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.13), // 왼쪽 패딩 추가
+              child: const Text(
+                'Google로 시작하기',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
               ),
             ),
           ],
@@ -96,28 +99,31 @@ class KakaoLoginButton extends StatelessWidget {
           }
         }
       },
-
-      color: Colors.yellow, // 카카오 컬러로 변경 가능
+      color: Color(0xFFFEE500), // 카카오 컬러로 변경 가능
       elevation: 3,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
+        borderRadius: BorderRadius.circular(10),
         side: const BorderSide(color: Colors.grey),
       ),
-      child: const Padding(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: SizedBox( // 버튼 크기를 조정하기 위해 SizedBox로 감싸기
+        height: 50, // 버튼의 높이 설정
+        width: MediaQuery.of(context).size.width * 0.8, // 화면 너비의 80%로 설정
         child: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Icon(
-              Icons.add_circle_outline,
-              color: Colors.black, // 카카오 컬러로 변경 가능
+            SizedBox( // 이미지를 표시하기 위해 SizedBox로 감싸기
+              width: MediaQuery.of(context).size.width * 0.1, // 화면 너비의 10%로 설정
+              height: MediaQuery.of(context).size.width * 0.1, // 화면 너비의 10%로 설정
+              child: Image.asset('assets/kakaologo.png'), // 이미지 추가
             ),
-            SizedBox(width: 10),
-            Text(
-              '카카오 로그인',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black87,
+            const SizedBox(width: 10), // 이미지와 텍스트 사이의 간격 조정
+            Padding(
+              padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.13), // 왼쪽 패딩 추가
+              child: const Text(
+                'Kakao로 시작하기',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
               ),
             ),
           ],
